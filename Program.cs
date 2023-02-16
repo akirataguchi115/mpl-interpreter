a9eb7f4560a6a54f6136f309716c89ef826e5497
Akira Taguchi <akirahattivatti@gmail.com>
2023-02-16 09:48:16 +0200

    Scan tokens into list w/o newlines

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+
+        static string ScanString(string literal)
+        {
+            int index = 0;
+            while (index < literal.Length)
+            {
+                Console.WriteLine(literal[index]);
+                if (literal[index].Equals('\\'))
+                {
+                    return "found escape character!";
+                }
+                index++;
+            }
+            return literal;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Mini-PL!");
+            string source = System.IO.File.ReadAllText(args[0]);
+            List<List<String>> tokens = new List<List<String>>();
+            tokens.Add(new List<string>());
+            int line = 0;
+            int index = 0;
+            string token = "";
+            while (index != source.Length)
+            {
+                if (source[index] == char.Parse(" "))
+                {
+                    tokens[line].Add(token);
+                    token = "";
+                    index++;
+                }
+                token += source[index];
+                index++;
+            }
+            tokens[line].Add(token);
+            Console.WriteLine("Parsed tokens");
+            foreach (var parsedToken in tokens[0])
+            {
+                Console.WriteLine(parsedToken);
+            }
+        }
+    }
+}
